b4200a3e94b31a22bb467a4ab77a4af145bbfad3
Abdul razak <abdulrazak.nasriudeen@gmail.com>
2025-11-08 19:31:15 +0530

    fix(build)

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,10 @@
 /* ------------------------------------------------------------------ */
 /*                          COMPANY LOGOS                             */
 /* ------------------------------------------------------------------ */
-import lagoonTech from "./companies/lagoon-technology.png";
-import chauffeurUAE from "./companies/chauffeur-UAE.png";
-import fcFleet from "./clients/fcfleet.png";
-import goyaka from "./companies/goyaka.png";
+import lagoonTech from "./companies/lagoon-technology-temp.png";
+import chauffeurUAE from "./companies/chauffeur-UAE-temp.png";
+import fcFleet from "./clients/fcfleet-temp.png";
+import goyaka from "./companies/goyaka-temp.png";
 
 export const Companies = {
 	lagoonTech,
@@ -16,13 +16,13 @@ export const Companies = {
 /* ------------------------------------------------------------------ */
 /*                            BRAND LOGOS                             */
 /* ------------------------------------------------------------------ */
-import boobaTaxi from "./brands/boobaTaxi.png";
-import boobaZhop from "./brands/BoobaZhop.png";
-import jezX from "./brands/jezX.png";
-import ricSpace from "./brands/Ricspace.png";
-import enyard from "./brands/Enyard.png";
-import nexorX from "./brands/nexora.png";
-import azra from "./brands/Azra.png";
+import boobaTaxi from "./brands/boobaTaxi-temp.png";
+import boobaZhop from "./brands/BoobaZhop-temp.png";
+import jezX from "./brands/jezX-temp.png";
+import ricSpace from "./brands/Ricspace-temp.png";
+import enyard from "./brands/Enyard-temp.png";
+import nexorX from "./brands/nexora-temp.png";
+import azra from "./brands/Azra-temp.png";
 
 export const Brands = {
 	boobaTaxi,
@@ -37,13 +37,13 @@ export const Brands = {
 /* ------------------------------------------------------------------ */
 /*                            CLIENT LOGOS                            */
 /* ------------------------------------------------------------------ */
-import adarsh from "./clients/adarsh.png";
-import dssc from "./clients/dssc.png";
-import lgConstruction from "./clients/LG construction.png";
-import ramakrishnaCollege from "./clients/ramakrishnaClg.png";
-import rojavanamSchool from "./clients/rojavanam.png";
-import sidone from "./clients/sidone.png";
-import chicago from "./clients/Chicago.png";
+import adarsh from "./clients/adarsh-temp.png";
+import dssc from "./clients/dssc-temp.png";
+import lgConstruction from "./clients/LG construction-temp.png";
+import ramakrishnaCollege from "./clients/ramakrishnaClg-temp.png";
+import rojavanamSchool from "./clients/rojavanam-temp.png";
+import sidone from "./clients/sidone-temp.png";
+import chicago from "./clients/Chicago-temp.png";
 
 export const ClientsLogo = {
 	adarsh,
@@ -59,10 +59,10 @@ export const ClientsLogo = {
 /* ------------------------------------------------------------------ */
 /*                            LEADERSHIP                              */
 /* ------------------------------------------------------------------ */
-import arshitha from "./leaders/Arshitha.png";
-import jibisha from "./leaders/Jibisha.png";
-import jinuMartin from "./leaders/JinuMartin.png";
-import shabin from "./leaders/shabin.png";
+import arshitha from "./leaders/Arshitha-temp.png";
+import jibisha from "./leaders/Jibisha-temp.png";
+import jinuMartin from "./leaders/JinuMartin-temp.png";
+import shabin from "./leaders/shabin-temp.png";
 
 export const Leaders = {
 	arshitha,
@@ -74,8 +74,8 @@ export const Leaders = {
 /* ------------------------------------------------------------------ */
 /*                            SOCIAL ICONS                            */
 /* ------------------------------------------------------------------ */
-import linkedin from "./social/linkedinLogo.png";
-import teamWork from "./social/TeamWork.jpg";
+import linkedin from "./social/linkedinLogo-temp.png";
+import teamWork from "./social/TeamWork-temp.jpg";
 
 export const Social = {
 	linkedin,
